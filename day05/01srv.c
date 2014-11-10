03dc6cbf27b04bdf397779d0077fe5ab5aca79d5
xsun <sunxin89@gmail.com>
2014-11-10 15:49:36 -0800

    add day05

---
+++ day05/01srv.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+int main()
+{
+    int sockfd = 0;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0)
+    {
+        perror("func sock");
+        exit(0);
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    struct in_addr inAddr;
+    memset(&inAddr, 0, sizeof(inAddr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8008);
+    inet_aton("120.0.0.1", &inAddr);
+    addr.sin_addr = inAddr;
+
+    if(bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("func bind");
+        exit(0);
+    }
+    if(listen(sockfd, SOMAXCONN) < 0)
+    {
+        perror("func listen");
+        exit(0);
+    }
+
+    struct sockaddr_in peerAddr;
+    socklen_t peerAddrLen = sizeof(peerAddr);
+    int conn = 0;
+    if((conn = accept(sockfd, (struct sockaddr *)&peerAddr, (socklen_t *)&peerAddrLen)))
+    {
+        perror("func accept");
+        exit(0);
+    }
+    printf("peer address:%s \t peerport:%d\n", inet_ntoa(peerAddr.sin_addr), ntohs(peerAddr.sin_port));
+    char buf[1024] = {0};
+    while(1)
+    {
+        int ret = read(conn, buf, sizeof(buf));
+        if(ret == 0)
+        {
+            printf("peer reset");
+            exit(0);
+        }else if(ret < 0)
+        {
+            perror("read fail");
+        }
+
+        fputs(buf, stdout);
+        write(conn, buf, sizeof(buf));
+    }
+    return 0;
+}
